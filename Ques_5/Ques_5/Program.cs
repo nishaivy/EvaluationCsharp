c0fc85c820cb27c63d6035c3c5e7441a8c8ce245
nisha hiremani <113030466+nishaivy@users.noreply.github.com>
2022-10-07 20:36:46 +0530

    Update Program.cs
    add factorial

--- Ques_5/Ques_5/Program.cs
+++ Ques_5/Ques_5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 public class Ques_5
 {
     public static void Main()
@@ -6,11 +6,16 @@ public class Ques_5
         int j, n;
         Console.Write("Enter the number : \n");
         n = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Display the multiplication table:\n");
+        Console.Write("the multiplication table:\n");
         for (j = 1; j <= 10; j++)
         {
             Console.Write("{0} X {1} = {2} \n", n, j, n * j);
         }
+        
+        int i,f=1;
+        for(i=1;i<=n;i++)
+          f=f*i;
+          Console.Write("The Factorial of {0} is: {1}\n",n,f);
         Console.ReadKey();
     }
 }
